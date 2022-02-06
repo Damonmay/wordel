e6d6a2950f67fc2ab259acbf9fc548c3a9e5f09d
Damon May <damonmay3@gmail.com>
2022-02-06 13:37:54 -0500

    better UI

--- wordle.py
+++ wordle.py
@@ -1,8 +1,5 @@
 # wordle game
 
-
-#link to a list of 5 letter words
-
 import random
 
 
@@ -17,19 +14,12 @@ print("-all guesses should be lower case letters")
 print("-there can be double letters")
 
 
-
-
-
-# words = ["apple", "pears", "goose", "floor"]
-# real_word = random.choice(words)
 real_word_split = split(real_word) 
 guess_word = "nope"
 guessed_letters = ["z"]
 real_word_progress = ["_","_","_","_","_"]
 found_letters = []
 
-# print("real word", real_word)
-# print(real_word_split)
 
 trials = 0
 while((guess_word != real_word) and (trials < 10)):
@@ -37,22 +27,19 @@ while((guess_word != real_word) and (trials < 10)):
 	guess_word_split = split(guess_word)
 	
 	if(len(guess_word_split) != 5):
-		print("too long try again")
+		print("this has to be a 5 letter word")
 	else:
 		i = 0
 		for x in guess_word:
-			if(x == real_word_split[i]):
-				# print(x, "is in the correct spot")
+			if(x == real_word_split[i]): #correct spot
 				real_word_progress[i] = x
-			elif(x in real_word_split):
-				# print(x, "is in the word, wrong spot")
-				found_letters.append(x)
-			else:
-				# print(x," is not in the word")
+			elif(x in real_word_split): #is in the word, but wrong spot
+				if(x not in found_letters):
+					found_letters.append(x)
+			else: #letter is not in the word
 				if(x not in guessed_letters):
 					guessed_letters.append(x)
 			i = i+1
-	# top_line = ["Q", "W", "E", "R", "T", "Y"]
 	print("Letters not in the word", guessed_letters)
 	print(real_word_progress)
 	print("Letters in the word", found_letters)
